1893132836f8a835599a40648327453b989228f8
Nova Subhan <148331004+NohanX7@users.noreply.github.com>
2023-10-22 23:16:02 +0700

    Add files via upload

---
+++ LIMAS SEGITIGA.py
@@ -0,0 +1,25 @@
+print("Menghitung Luas dan Volume Limas Segitiga")
+
+# Variabel
+ls1 = 2
+ls2 = 3
+ls3 = 4
+ls4 = 5
+a = 6
+t = 7
+T = 8
+
+# Rumus
+luas = (ls1 + ls2 + ls3 + ls4)
+vol = (0.5 * a * t * T) // 3
+
+# Output
+print("Luas Sisi 1 : ", ls1)
+print("Luas Sisi 2 : ", ls2)
+print("Luas Sisi 3 : ", ls3)
+print("Luas Sisi 4 : ", ls4)
+print("Alas : ", a)
+print("Tinggi : ", t)
+print("Tinggi Limas : ", T)
+print("Luas Limas Segitiga : ", luas)
+print("Volume Limas Segitiga : ", vol)
